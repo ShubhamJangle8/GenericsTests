8b86ae69c2a59f8e1995a5007a5f60e33ebfdc25
jangle <shubhamjangale0000@gmail.com>
2020-10-11 20:55:32 +0530

    Refactor - Added Functionality to find the max String Value using Comparable and Generics

--- src/main/java/com/generics/genericstest/FindMax.java
+++ src/main/java/com/generics/genericstest/FindMax.java
@@ -1,23 +1,45 @@
 package com.generics.genericstest;
 
-public class FindMax {
-	
+public class FindMax<x extends Comparable<x>> {
+	x first, second, third;
+
 	/**
+	 * Constructor
+	 * 
+	 * @param first
+	 * @param second
+	 * @param third
+	 */
+	public FindMax(x first, x second, x third) {
+		this.first = first;
+		this.second = second;
+		this.third = third;
+	}
+
+	/**
+	 * UC 3 refactoring to use generic class
+	 * 
+	 * @return
+	 */
+	public x testMaximum() {
+		return testMaximum(first, second, third);
+	}
+
+	/**
+	 * UC 3 refactoring to use generic method
+	 * 
+	 * @param <E>
 	 * @param first
 	 * @param second
 	 * @param third
 	 * @return
 	 */
-	public Float find(Float first, Float second, Float third) {
-		if (first.compareTo(second) == 1 && first.compareTo(third) == 1) {
-			return first;
-		} 
-		else if (second.compareTo(third) == 1 && second.compareTo(first) == 1) {
-			return second;
-		} 
-		else {
-			return third;
-		}
+	private static <E extends Comparable<E>> E testMaximum(E firstVar, E secondVar, E thirdVar) {
+		if (firstVar.compareTo(secondVar) > 0 && firstVar.compareTo(thirdVar) > 0)
+			return firstVar;
+		else if (secondVar.compareTo(firstVar) > 0 && secondVar.compareTo(thirdVar) > 0)
+			return secondVar;
+		else
+			return thirdVar;
 	}
-
-} 	
+}
--- src/test/java/com/generics/genericstest/FindMaxTest.java
+++ src/test/java/com/generics/genericstest/FindMaxTest.java
@@ -1,40 +1,80 @@
 package com.generics.genericstest;
 
-import org.junit.Assert;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 class FindMaxTest {
-	static FindMax max;
 
-	public FindMaxTest() {
-		max = new FindMax();
-	}
-
-	/**
-	 * test for 1st number
-	 */
 	@Test
-	void test1() {
-		Float expected = 5.0f;
-		Assert.assertEquals(expected, max.find(5.0f, 2f, 3.0f));
+	public void givenFirstMaxInt_shouldReturn_first() {
+		Integer first = 9;
+		Integer second = 5;
+		Integer third = 7;
+		assertEquals(first, new FindMax<Integer>(first, second, third).testMaximum());
 	}
 
-	/**
-	 * test for 2nd number
-	 */
 	@Test
-	void test2() {
-		Float expected = 4f;
-		Assert.assertEquals(expected, max.find(1f, 4f, 3f));
+	public void givenSecondMaxInt_shouldReturn_second() {
+		Integer first = 9;
+		Integer second = 10;
+		Integer third = 7;
+		assertEquals(second, new FindMax<Integer>(first, second, third).testMaximum());
 	}
 
-	/**
-	 * test for 3rd number
-	 */
 	@Test
-	void test3() {
-		Float expected = 3f;
-		Assert.assertEquals(expected, max.find(1f, 2f, 3f));
+	public void givenThirdMaxInt_shouldReturn_third() {
+		Integer first = 9;
+		Integer second = 5;
+		Integer third = 10;
+		assertEquals(third, new FindMax<Integer>(first, second, third).testMaximum());
 	}
 
+	@Test
+	public void givenFirstMaxFloat_shouldReturn_first() {
+		Float first = 9.5f;
+		Float second = 5.4f;
+		Float third = 7.1f;
+		assertEquals(first, new FindMax<Float>(first, second, third).testMaximum());
+	}
+
+	@Test
+	public void givenSecondMaxFloat_shouldReturn_second() {
+		Float first = 9.5f;
+		Float second = 10.4f;
+		Float third = 7.1f;
+		assertEquals(second, new FindMax<Float>(first, second, third).testMaximum());
+	}
+
+	@Test
+	public void givenThirdMaxFloat_shouldReturn_third() {
+		Float first = 9.5f;
+		Float second = 5.4f;
+		Float third = 10.3f;
+		assertEquals(third, new FindMax<Float>(first, second, third).testMaximum());
+	}
+
+	@Test
+	public void givenFirstMaxString_shouldReturn_first() {
+		String first = "peach";
+		String second = "banana";
+		String third = "apple";
+		assertEquals(first, new FindMax<String>(first, second, third).testMaximum());
+	}
+
+	@Test
+	public void givenSecondMaxString_shouldReturn_second() {
+		String first = "banana";
+		String second = "peach";
+		String third = "apple";
+		assertEquals(second, new FindMax<String>(first, second, third).testMaximum());
+	}
+
+	@Test
+	public void givenThirdMaxString_shouldReturn_third() {
+		String first = "banana";
+		String second = "apple";
+		String third = "peach";
+		assertEquals(third, new FindMax<String>(first, second, third).testMaximum());
+	}
 }
