cca9866c80366b6fb958ed1f33f2d2409c08c030
jangle <shubhamjangale0000@gmail.com>
2020-10-12 07:30:09 +0530

    Refactored to print the maximum number using generics

--- src/test/java/com/generics/genericstest/FindMaxTest.java
+++ src/test/java/com/generics/genericstest/FindMaxTest.java
@@ -8,51 +8,24 @@ class FindMaxGenericsTest {
 
 	@Test
 	public void givenMultipleInt_shouldReturn_maxInteger() {
-		Integer first = 9;
-		Integer second = 5;
-		Integer third = 7;
-		Integer fourth = 10;
-		Integer fifth = 1;
-		Integer sixth = 2;
+		Integer first = 4;
+		Integer second = 50;
+		Integer third = 70;
+		Integer fourth = 100;
+		Integer fifth = 10;
+		Integer sixth = 20;
 		assertEquals(fourth, new FindMax<Integer>(first, second, third, fourth, fifth, sixth).testMaximum());
 	}
 
 	@Test
-	public void givenMultipeFloat_shouldReturn_maxFloat() {
-		Integer first = 9;
-		Integer second = 51;
-		Integer third = 7;
-		Integer fourth = 10;
-		assertEquals(second, new FindMax<Integer>(first, second, third, fourth).testMaximum());
-	}
-
-	@Test
 	public void givenMultipleFloat_shouldReturn_MaxFloat() {
-		Float first = 9.5f;
-		Float second = 5.4f;
-		Float third = 7.1f;
+		Float first = 9.54f;
+		Float second = 7.4f;
+		Float third = 40.1f;
 		Float fourth = 10.5f;
-		Float fifth = 12.7f;
-		Float sixth = 20.7f;
+		Float fifth = 15.7f;
+		Float sixth = 200.7f;
 		assertEquals(sixth, new FindMax<Float>(first, second, third, fourth, fifth, sixth).testMaximum());
-	}
-
-	@Test
-	public void givenMultipeInt_shouldReturn_maxInt() {
-		Float first = 9.5f;
-		Float second = 5.4f;
-		Float third = 7.1f;
-		Float fourth = 10f;
-		assertEquals(fourth, new FindMax<Float>(first, second, third, fourth).testMaximum());
-	}
-
-	@Test
-	public void givenMultipeString_shouldReturn_maxString() {
-		String first = "peach";
-		String second = "banana";
-		String third = "apple";
-		String fourth = "coconut";
-		assertEquals(first, new FindMax<String>(first, second, third, fourth).testMaximum());
 	}
 
 	@Test
@@ -62,7 +35,7 @@ class FindMaxGenericsTest {
 		String third = "apple";
 		String fourth = "coconut";
 		String fifth = "mango";
-		String sixth = "santra";
-		assertEquals(sixth, new FindMax<String>(first, second, third, fourth, fifth, sixth).testMaximum());
+		String sixth = "orange";
+		assertEquals(first, new FindMax<String>(first, second, third, fourth, fifth, sixth).testMaximum());
 	}
 }
